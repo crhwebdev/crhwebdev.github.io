f4cb87937d8d2c7af7df9e2f8ea1828d9fdfee1b
Carl Hain <crhain@gmail.com>
2019-09-12 10:51:04 -0500

    Set up basic structure of gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
-function defaultTask(cb) {
+const { watch, src, dest, series, parallel } = require('gulp');
+
+function javascript(cb) {
+  console.log('running javascript processes...');
   cb();
 }
 
+function css(cb) {
+  console.log('running css processes...');
+  cb();
+}
+
+function dev(cb) {
+  cb();
+}
+
+function defaultTask(cb) {
+  parallel(javascript, css);
+  cb();
+}
+
+exports.dev = dev;
 exports.default = defaultTask;
