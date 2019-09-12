1d37246f20b07983a56a585eed1950503ff1058c
Carl Hain <crhain@gmail.com>
2019-09-12 11:54:36 -0500

    Update to use gulp instead of npm scripts for processing css and js files
    Change links in index.html to point to correct locations for css files; refactor gulpfile.js to use more modern syntax
    
    gulpfile.js is now configured to process css by compiling sass files, autoprefixing css, and then minifying it; installed plugins for postcss, cssnano, and autoprefixer
    
    Add javascript uglifying to gulpfile.js
    
    Add gulp watch for javascript file changes
    
    Modify gulpfile.js to make css and js tasks public
    
    uninstall uneeded npm packages
    
    Update package.json scripts to use gulp now
    
    Add gulp rename plugin
    
    Replace npm scripts with gulp and change project file structure to include src and public folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,42 @@
-const { watch, src, dest, series, parallel } = require('gulp');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
-function javascript(cb) {
-  console.log('running javascript processes...');
-  cb();
+sass.compiler = require('node-sass');
+
+function js() {
+  return gulp
+    .src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'));
 }
 
-function css(cb) {
-  console.log('running css processes...');
-  cb();
+function css() {
+  const plugins = [
+    cssnano,
+    autoprefixer({ browsers: ['> 1%'], cascade: false })
+  ];
+
+  return gulp
+    .src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('./public/css'));
 }
 
-function dev(cb) {
-  cb();
+function watchcss() {
+  gulp.watch('./src/sass/**/*.scss', css);
 }
 
-function defaultTask(cb) {
-  parallel(javascript, css);
-  cb();
+function watchjs() {
+  gulp.watch('./src/js/**/*.js', js);
 }
 
-exports.dev = dev;
-exports.default = defaultTask;
+exports.css = css;
+exports.js = js;
+exports.dev = gulp.parallel(watchcss, watchjs);
+exports.default = gulp.parallel(css, js);
